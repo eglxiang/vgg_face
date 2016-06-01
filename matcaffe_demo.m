bd28c334206a8daa76e6dce47a07e16bf19f6816
eglxiang <cnwhu@yahoo.com>
2016-06-01 15:25:21 -0400

    Add matlab script for running VGG_face

---
+++ matcaffe_demo.m
@@ -0,0 +1,26 @@
+%  Copyright (c) 2015, Omkar M. Parkhi
+%  All rights reserved.
+addpath('/usr/local/matlab')
+addpath('/root/ws/dl/caffe/matlab/')
+img = imread('ak.png');
+img = single(img);
+
+averageImg = [129.1863,104.7624,93.5940] ;
+
+img = cat(3,img(:,:,1)-averageImg(1),...
+    img(:,:,2)-averageImg(2),...
+    img(:,:,3)-averageImg(3));
+
+img = img(:, :, [3, 2, 1]); % convert from RGB to BGR
+img = permute(img, [2, 1, 3]); % permute width and height
+
+model = 'VGG_FACE_deploy.prototxt';
+weights = 'VGG_FACE.caffemodel';
+caffe.set_mode_cpu();
+net = caffe.Net(model, weights, 'test'); % create net and load weights
+
+tic
+res = net.forward({img});
+prob = res{1};
+toc
+caffe_ft = net.blobs('fc7').get_data();
